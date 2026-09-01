12ecea0338cba700de45055ec089ddb0f9dc2a80
42zhangjing <32725852+42zhangjing@users.noreply.github.com>
2026-09-01 09:25:13 +0800

    Prepare v0.2.3 prerelease

--- build-app.sh
+++ build-app.sh
@@ -54,9 +54,9 @@ cat > "$APP/Contents/Info.plist" <<PLIST
   <key>CFBundleDisplayName</key>
   <string>$APP_NAME</string>
   <key>CFBundleShortVersionString</key>
-  <string>0.2.2</string>
+  <string>0.2.3</string>
   <key>CFBundleVersion</key>
-  <string>4</string>
+  <string>5</string>
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>LSUIElement</key>
